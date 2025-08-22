f58935c75bfed9ebbb42c2845c4d922da2cae1d4
DohouFawase <d.fawase@softvodooz.com>
2025-08-22 13:25:38 +0200

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavBar from "@/components/shared/navBar";
+import FooterNewsletter from "@/components/shared/footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavBar />
         {children}
+        <FooterNewsletter />
       </body>
     </html>
   );
---
+++ src/mock/data.ts
@@ -0,0 +1,31 @@
+export const Navlink = [
+    {
+        id: 1,
+        name: 'Acceuil',
+        url: ''
+    },
+
+    {
+        id: 2,
+        name: 'Solution',
+        url: ''
+    },
+
+    {
+        id: 3,
+        name: 'A propos',
+        url: ''
+    },
+
+    {
+        id: 4,
+        name: 'Services',
+        url: ''
+    },
+
+    {
+        id: 5,
+        name: 'Blog',
+        url: ''
+    }
+]
